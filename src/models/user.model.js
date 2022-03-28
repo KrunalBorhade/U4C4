411b17b2bff1eb8ba4a252c552c8a8c0a2098cba
KrunalBorhade <krunalborhade2897@gmail.com>
2022-03-28 10:31:52 +0530

    userModel changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+
 
 const userSchema = new mongoose.Schema(
     {
@@ -12,6 +14,15 @@ const userSchema = new mongoose.Schema(
         timestamps:true
     }
 )
+userSchema.pre("save", function (next){
+    const hash = bcrypt.hashSync(this.password, 6);
+    this.password = hash
+    return next()
+})
+
+userSchema.methods.checkPassword = function(password){
+    return bcrypt.compareSync(password, hash);
+}
 
 const User = mongoose.model("user",userSchema);
 
